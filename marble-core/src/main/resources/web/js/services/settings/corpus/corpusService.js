5fee4393b3876528bec7316f532383dcc3702792
miguelfc <github@miguelfc.com>
2016-12-02 19:53:23 +0100

    Removed corpus options from UI.

--- marble-core/src/main/resources/web/js/services/settings/corpus/corpusService.js
+++
@@ -1,9 +0,0 @@
-angular.module('marbleCoreApp')
-.factory('CorpusSenticnetFactory', ['$resource', 'getInterceptor', CorpusSenticnetFactory]);
-
-function CorpusSenticnetFactory($resource, getInterceptor) {
-    return $resource('/api/corpus/senticnet', {}, {
-        query: {method: 'GET', isArray: false, interceptor: getInterceptor},
-        create: { method: 'POST', interceptor: getInterceptor }
-    })
-};
